0288c4c7860c18a9267507be616293aac370acc7
Madd0c101 <newdagon@mail.ru>
2021-02-07 23:39:45 +0200

    2-Checked again

--- tracker-core/src/main/java/jdev.tracker/services/Jform.java
+++ tracker-core/src/main/java/jdev.tracker/services/Jform.java
@@ -1,12 +1,12 @@
 package jdev.tracker.services;
 import jdev.dto.PointDTO;
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class Jform {
 
-  //  private static String expected;
+    private static String expected;
    // public String AutoId;
     public static PointDTO point= new PointDTO();
 
@@ -16,8 +16,8 @@ public class Jform {
         //    point.setLat(45);
     }
     public static String fromJson() throws Exception {
-   //     ObjectMapper mapper = new ObjectMapper();
-     //   PointDTO dto=mapper.readValue(expected,PointDTO.class);
+       // ObjectMapper mapper = new ObjectMapper();
+      // PointDTO dto=mapper.readValue(point.getAutoId(),PointDTO.class);
         return point.getAutoId();
     }
 }
